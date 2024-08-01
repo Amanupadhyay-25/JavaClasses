e213598d9ba06117e53190f9813da8508b5e065f
Aman Upadhyay <146805438+Amanupadhyay-25@users.noreply.github.com>
2024-08-01 10:27:21 +0530

    objcreationusingreferencevariable
    this is the example of creation of object using reference variable

---
+++ Animal1.java
@@ -0,0 +1,11 @@
+class Animal1{
+  String color; 
+  int age;
+ public static void main(String[] args){
+   Animal1 buzo=new Animal1();
+   buzo.color="Black";
+   buzo.age=26;
+System.out.println(buzo.color+" "+buzo.age);
+}
+
+}

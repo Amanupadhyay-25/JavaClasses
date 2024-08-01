a62d7331648fb93ba5ede3aae925cfd532fd6fc9
Aman Upadhyay <146805438+Amanupadhyay-25@users.noreply.github.com>
2024-08-02 05:29:22 +0530

    NonParamConstruct

---
+++ OOPS/NonParamConstruct.java
@@ -0,0 +1,11 @@
+public class NonParamConstruct{
+
+public NonParamConstruct(){
+  System.out.println("Hey there this side Aman Upadhyay");
+}
+public static void main(String[] args){
+  NonParamConstruct t=new NonParamConstruct();
+ System.out.println(" I used to live in Ghaziabad");
+}
+}
+
